65da52f88bdc84c06c378b50bb91afc0b8d7c304
islomg <gofurjonovislombek123@email.com>
2026-09-19 14:42:59 +0500

    second commit

--- config/settings.py
+++ config/settings.py
@@ -2,13 +2,16 @@ from pathlib import Path
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
 
-SECRET_KEY = "django-insecure-instudy-crm-development-key"
-DEBUG = True
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-dev-key")
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", 'umarbek-backend-production.up.railway.app', 'lc-crm.uz']
+CSRF_TRUSTED_ORIGINS = ["https://*.up.railway.app"]
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -56,10 +59,10 @@ TEMPLATES = [{
 WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = []
